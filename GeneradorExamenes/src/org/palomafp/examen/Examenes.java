f985aabefa270ff01b999aea349cf248238c8ce4
Arturo <Arturo@Informatica>
2025-05-05 10:39:15 +0200

    Cambio de rutas

--- GeneradorExamenes/src/org/palomafp/examen/Examenes.java
+++ GeneradorExamenes/src/org/palomafp/examen/Examenes.java
@@ -4,8 +4,8 @@ public class Examenes {
 
     public static void main(String[] args) throws Exception {
     	GestorExamenes gestor=new GestorExamenes();
-    	gestor.cargarBancoPreguntasCSV("/config/preguntas.csv");
-    	gestor.cargarAlumnos("/config/Alumnos_RA.csv");
+    	gestor.cargarBancoPreguntasCSV("/preguntas.csv");
+    	gestor.cargarAlumnos("/Alumnos_RA.csv");
     	gestor.generarExamenes();
     }
 
